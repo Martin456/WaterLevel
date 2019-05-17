1eced7f6a8a56064d17d20dfd63dd807688f54af
Martin Fous <fous.martin@gmail.com>
2019-05-17 15:43:13 +0200

    Add tests and input validation

--- src/main/scala/WaterLevel.scala
+++ src/main/scala/WaterLevel.scala
@@ -14,24 +14,27 @@ object WaterLevel {
   }
 
   def getMaxVolume(inp: List[Int]): Int = {
-
-    @tailrec
-    def recVolumeCalculation(input: List[Int], length: Int, maxVol: Int): Int = {
-      if (length == 0) maxVol
-      else {
-        val volumes = for {
-          x <- 0 until input.length
-          if input.length > x + length
-        } yield calculateVolume(input(x), input(x + length), length)
-        val newMax = volumes.max
-        if (newMax >= maxVol) recVolumeCalculation(input, length - 1, newMax)
-        else recVolumeCalculation(input, length - 1, maxVol)
+    if(inp.length < 2) throw new IllegalArgumentException("Input list contains less than two elements")
+    else if (inp.exists(_ < 0)) throw new IllegalArgumentException("Input list contains negative numbers")
+    else {
+      @tailrec
+      def recVolumeCalculation(input: List[Int], length: Int, maxVol: Int): Int = {
+        if (length == 0) maxVol
+        else {
+          val volumes = for {
+            x <- 0 until input.length
+            if input.length > x + length
+          } yield calculateVolume(input(x), input(x + length), length)
+          val newMax = volumes.max
+          if (newMax >= maxVol) recVolumeCalculation(input, length - 1, newMax)
+          else recVolumeCalculation(input, length - 1, maxVol)
+        }
       }
+
+      def calculateVolume(x: Int, y: Int, length: Int): Int = Math.min(x, y) * length
+
+      recVolumeCalculation(inp, inp.length - 1, 0)
     }
-
-    def calculateVolume(x: Int, y: Int, length: Int): Int = Math.min(x, y) * length
-
-    recVolumeCalculation(inp, inp.length - 1, 0)
   }
 
 
